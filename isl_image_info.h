ab480549e6076fef45f5f9a13d2dcea7ad9d3598
Ilya Kolbin <iskolbin@gmail.com>
2016-12-15 16:33:38 +0300

    renamed fread* to islii__fread*

--- isl_image_info.h
+++ isl_image_info.h
@@ -78,40 +78,40 @@ islii_result islii_info_file( FILE *f ) {
 }
 #endif
 
-static unsigned char fread_u8( islii_file f, islii_fread read ) {
+static unsigned char islii__fread_u8( islii_file f, islii_fread read ) {
 	unsigned char a;
 	read( &a, 1, 1, f );
 	return a;
 }
 
-static unsigned int fread_u16_little( islii_file f, islii_fread read ) {
-	return fread_u8(f,read) + (fread_u8(f,read)<<8);
+static unsigned int islii__fread_u16_little( islii_file f, islii_fread read ) {
+	return islii__fread_u8(f,read) + (fread_u8(f,read)<<8);
 }
 
-static unsigned int fread_u32_little( islii_file f, islii_fread read ) {
-	return fread_u8(f,read) + (fread_u8(f,read)<<8) + (fread_u8(f,read)<<16) + (fread_u8(f,read)<<24);
+static unsigned int islii__fread_u32_little( islii_file f, islii_fread read ) {
+	return islii__fread_u8(f,read) + (fread_u8(f,read)<<8) + (fread_u8(f,read)<<16) + (fread_u8(f,read)<<24);
 }
 
-static unsigned int fread_u16_big( islii_file f, islii_fread read ) {
-	return (fread_u8(f,read)<<8) + fread_u8(f,read);
+static unsigned int islii__fread_u16_big( islii_file f, islii_fread read ) {
+	return (islii__fread_u8(f,read)<<8) + fread_u8(f,read);
 }
 
-static unsigned long int fread_u32_big( islii_file f, islii_fread read ) {
-	return (fread_u8(f,read)<<24) + (fread_u8(f,read)<<16) + (fread_u8(f,read)<<8) + fread_u8(f,read);
+static unsigned long int islii__fread_u32_big( islii_file f, islii_fread read ) {
+	return (islii__fread_u8(f,read)<<24) + (fread_u8(f,read)<<16) + (fread_u8(f,read)<<8) + fread_u8(f,read);
 }
 
-static unsigned int fread_u16( islii_file f, islii_fread read, int big ) {
-	return big ? fread_u16_big(f,read) : fread_u16_little(f,read);
+static unsigned int islii__fread_u16( islii_file f, islii_fread read, int big ) {
+	return big ? islii__fread_u16_big(f,read) : fread_u16_little(f,read);
 }
 
-static unsigned long int fread_u32( islii_file f, islii_fread read, int big ) {
-	return big ? fread_u32_big(f,read) : fread_u32_little(f,read);
+static unsigned long int islii__fread_u32( islii_file f, islii_fread read, int big ) {
+	return big ? islii__fread_u32_big(f,read) : fread_u32_little(f,read);
 }
 
 static void fskip( islii_file f, islii_fread read, int count ) {
 	int i;
 	for ( i = 0; i < count; i++ ) {
-		fread_u8( f, read );
+		islii__fread_u8( f, read );
 	}
 }
 
@@ -121,11 +121,11 @@ static islii_result fread_png( islii_file f, islii_fread read ) {
 	fread( &third, 1, 1, f );
 	if ( third == 78 ) {
 		fskip( f, read, 13 );
-		result.width = fread_u32_big( f, read );
-		result.height = fread_u32_big( f, read );
+		result.width = islii__fread_u32_big( f, read );
+		result.height = islii__fread_u32_big( f, read );
 		result.format = ISLII_PNG;
-		result.bpp = (unsigned int) fread_u8( f, read );
-		switch ( fread_u8( f, read )) {
+		result.bpp = (unsigned int) islii__fread_u8( f, read );
+		switch ( islii__fread_u8( f, read )) {
 			case 0: result.colortype = ISLII_GRAYSCALE; break;
 			case 2: result.colortype = ISLII_RGB; break;
 			case 3: result.colortype = ISLII_INDEX; break;
@@ -141,10 +141,10 @@ static islii_result fread_png( islii_file f, islii_fread read ) {
 static islii_result fread_bmp( islii_file f, islii_fread read ) {
 	islii_result result = { ISLII_ERROR_OK, ISLII_BMP, ISLII_UNKNOWN_COLORTYPE, 0, 0, 0 };
 	fskip( f, read, 16 );
-	result.width = fread_u32_little( f, read );
-	result.height = fread_u32_little( f, read );
-	fread_u16_little( f, read );
-	result.bpp = fread_u16_little( f, read );
+	result.width = islii__fread_u32_little( f, read );
+	result.height = islii__fread_u32_little( f, read );
+	islii__fread_u16_little( f, read );
+	result.bpp = islii__fread_u16_little( f, read );
 	switch ( result.bpp ) {
 		case 1: case 2: case 4: case 8: result.colortype = ISLII_INDEX; break;
 		case 24: result.colortype = ISLII_RGB; break;
@@ -155,10 +155,10 @@ static islii_result fread_bmp( islii_file f, islii_fread read ) {
 
 static islii_result fread_gif( islii_file f, islii_fread read ) {
 	islii_result result = { ISLII_ERROR_OK, ISLII_UNKNOWN, ISLII_UNKNOWN_COLORTYPE, 0, 0, 0 };
-	if ( fread_u8( f, read ) == 'F' ) {
+	if ( islii__fread_u8( f, read ) == 'F' ) {
 		fskip( f, read, 3 );
-		result.width = fread_u16_little( f, read );
-		result.height = fread_u16_little( f, read );
+		result.width = islii__fread_u16_little( f, read );
+		result.height = islii__fread_u16_little( f, read );
 		result.format = ISLII_GIF;
 		result.bpp = 8;
 		result.colortype = ISLII_INDEX;
@@ -170,28 +170,28 @@ static islii_result fread_gif( islii_file f, islii_fread read ) {
 
 static islii_result fread_jpeg( islii_file f, islii_fread read ) {
 	islii_result result = { ISLII_ERROR_OK, ISLII_JPEG, ISLII_UNKNOWN_COLORTYPE, 0, 0, 0 };
-	for(;;) switch (fread_u16_big( f, read )) {
+	for(;;) switch (islii__fread_u16_big( f, read )) {
 		case 0xffd8: case 0xffd0: case 0xffd1: case 0xffd2: case 0xffd3:
 		case 0xffd4: case 0xffd5: case 0xffd6: case 0xffd7: case 0xffd9:
 			break;
 
 		case 0xffdd:
-			fread_u16_big( f, read );
+			islii__fread_u16_big( f, read );
 			break;
 
 		case 0xffe0: case 0xffe1: case 0xffe2: case 0xffe3: case 0xffe4:
 		case 0xffe5: case 0xffe6: case 0xffe7: case 0xffe8: case 0xffe9: 
 		case 0xffea: case 0xffeb: case 0xffec: case 0xffed: case 0xffee:
 		case 0xffef: case 0xfffe: case 0xffdb: case 0xffc4: case 0xffda: 
-			fskip( f, read, ((long int) fread_u16_big( f, read )) - 2 );
+			fskip( f, read, ((long int) islii__fread_u16_big( f, read )) - 2 );
 			break;
 
 		case 0xffc0: case 0xffc2:
 			fskip( f, read, 2 );
-			result.bpp = (unsigned int) fread_u8( f, read );
-			result.width = fread_u16_big( f, read );
-			result.height = fread_u16_big( f, read );
-			switch ( fread_u8( f, read )) {
+			result.bpp = (unsigned int) islii__fread_u8( f, read );
+			result.width = islii__fread_u16_big( f, read );
+			result.height = islii__fread_u16_big( f, read );
+			switch ( islii__fread_u8( f, read )) {
 				case 1: result.colortype = ISLII_GRAYSCALE; break;
 				case 3: case 4: result.colortype = ISLII_RGB; break;
 			}
@@ -210,7 +210,7 @@ islii_result islii_info( islii_file f, islii_fread read ) {
 		return result;
 	} else {
 		islii_result result = { ISLII_ERROR_UNKNOWN_FORMAT, ISLII_UNKNOWN, ISLII_UNKNOWN_COLORTYPE, 0, 0, 0};
-		switch ( fread_u16_big( f, read )) {
+		switch ( islii__fread_u16_big( f, read )) {
 			case 0x8950: result = fread_png( f, read ); break;
 			case 0x424D: result = fread_bmp( f, read ); break;
 			case 0x4749: result = fread_gif( f, read ); break;
